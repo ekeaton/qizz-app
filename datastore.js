ac1de3648f97c3a890d486815d60f6165f955922
Estelle K <e.keaton@live.com>
2019-02-13 11:12:43 -0800

    added datastore.js

---
+++ datastore.js
@@ -0,0 +1,102 @@
+const STORE = [
+    {
+      question: 'How many species of giraffe are there?',
+      answers: [
+        'One',
+        'Five',
+        'Twelve',
+        'Ten'
+      ],
+      correctAnswer: 'One'
+    },
+    {
+    question: 'Where do giraffes live?',
+    answers: [
+      'South America',
+        'Africa',
+        'Asia',
+        'Alaska'
+    ],
+    correctAnswer: 'Africa'
+    },
+    {
+      question: 'What kind of terrain do giraffes prefer?',
+      answers: [
+        'Rocky slopes',
+          'Dense forests',
+          'Freshwater marshes',
+          'Open grassy plains'
+        ],
+        correctAnswer:'Open grassy plains'
+    },
+    {
+      question: 'To what height do male giraffes grow?',
+      answers: [
+        '15 feet(4.6 m)',
+          '21 feet(6.4 m)',
+          '18 feet(5.5 m)',
+          '13 feet(3.9 m)'
+         ],
+         correctAnswer: '18 feet(5.5 m)'
+    },
+    {
+      question: 'To what height do female giraffes grow?',
+      answers: [
+        '15 feet (4.6 m)',
+          '18 feet (5.5 m)',
+          '12 feet (3.7 m)',
+          '10 feet (3.0 m)'
+      ],
+      correctAnswer: '15 feet (4.6 m)'
+    },
+    {
+      question: 'How long is the giraffe’s neck?',
+      answers: [
+       'Seven feet (2.1 m)',
+         'Six feet (1.8 m)',
+       'Nine feet (2.7 m)',
+         'Eight feet (2.4 m)'
+      ],
+      correctAnswer: 'Seven feet (2.1 m)'
+    },
+    {
+      question: 'Which of the giraffes has horns?',
+      answers: [
+        'Only Masai male giraffes',
+          'Neither sex',
+          'Certain types of adult Transvaal giraffe',
+          'Both sexes'
+      ],
+      correctAnswer: 'Both sexes'
+    },
+    {
+      question: 'With such a long neck and long legs, how does the giraffe drink water?',
+      answers: [
+        'Neck is long enough',
+        'Spreads its front legs',
+          'Kneels',
+        'Giraffes do not drink water'
+      ],
+      correctAnswer: 'Spreads its front legs'
+    },
+    {
+      question: 'At what speed can giraffes run?',
+      answers: [
+        '24 miles per hour (38 km/h)',
+          '29 miles per hour (47 km/h)',
+          '35 miles per hour (56 km/h)',
+          '32 miles per hour (51 km/h)'
+      ],
+      correctAnswer: '29 miles per hour (47 km/h)'
+    },
+    {
+      question: 'What are the social habits of giraffes?',
+      answers: [
+        'They live in herds',
+          'They only group together when danger is present',
+          'They live in pairs',
+          'Except for mating season they are usually alone'
+      ],
+      correctAnswer: 'They live in herds'
+    },
+    ];
